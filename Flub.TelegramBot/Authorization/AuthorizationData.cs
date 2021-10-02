00c4fa58ee258d5d015241590e33acf5e338936a
Luca Stäheli <luca.staeheli@bluewin.ch>
2021-10-02 03:10:51 +0200

    added authorization

---
+++ Flub.TelegramBot/Authorization/AuthorizationData.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json.Serialization;
+
+namespace Flub.TelegramBot.Authorization
+{
+    /// <summary>
+    /// Contains data about a user. The integrity can be verified with the <see cref="TelegramBot.ValidateAuthorizationData"/> method.
+    /// See <see href="https://core.telegram.org/widgets/login">Telegram Login Widget</see> for more informations.
+    /// </summary>
+    public class AuthorizationData
+    {
+        /// <summary>
+        /// Date of the authentication data was created in unix time format.
+        /// </summary>
+        [AuthenticationField]
+        [JsonPropertyName("auth_date")]
+        public long? AuthenticationDateValue { get; set; }
+        /// <summary>
+        /// Date of the authentication data was created.
+        /// </summary>
+        [JsonIgnore]
+        public DateTime? AuthenticationDate => AuthenticationDateValue.HasValue ? DateTimeOffset.FromUnixTimeSeconds(AuthenticationDateValue.Value).DateTime : null;
+        /// <summary>
+        /// First name of the user.
+        /// </summary>
+        [AuthenticationField]
+        [JsonPropertyName("first_name")]
+        public string FirstName { get; set; }
+        /// <summary>
+        /// The hash to be compared.
+        /// </summary>
+        [JsonPropertyName("hash")]
+        public string Hash { get; set; }
+        /// <summary>
+        /// Id of the user.
+        /// </summary>
+        [AuthenticationField]
+        [JsonPropertyName("id")]
+        public long? UserId { get; set; }
+        /// <summary>
+        /// Last name of the user.
+        /// </summary>
+        [AuthenticationField]
+        [JsonPropertyName("last_name")]
+        public string LastName { get; set; }
+        /// <summary>
+        /// Url to the profile picture of the user.
+        /// </summary>
+        [AuthenticationField]
+        [JsonPropertyName("photo_url")]
+        public Uri PhotoUrl { get; set; }
+        /// <summary>
+        /// Username of the user.
+        /// </summary>
+        [AuthenticationField]
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+        /// <summary>
+        /// Concatenation of all received fields.
+        /// </summary>
+        [JsonIgnore]
+        public string DataCheckString => string.Join('\n', typeof(AuthorizationData).GetProperties()
+            .Where(p => p.GetCustomAttributes<AuthenticationFieldAttribute>().Any())
+            .Select(p => p.GetCustomAttribute<JsonPropertyNameAttribute>() is JsonPropertyNameAttribute a && p.GetValue(this) is object value ? $"{a.Name}={value}" : null)
+            .Where(s => s is not null));
+
+
+        [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+        private sealed class AuthenticationFieldAttribute : Attribute
+        {
+
+        }
+    }
+}
---
+++ Flub.TelegramBot/Authorization/TelegramBotService.cs
@@ -0,0 +1,36 @@
+﻿using Flub.TelegramBot.Authorization;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Flub.TelegramBot
+{
+    public partial class TelegramBot
+    {
+        /// <summary>
+        /// Validates the speficied data.
+        /// </summary>
+        /// <param name="authorizationData">The data to be validated.</param>
+        /// <param name="throwExceptionOnFailure">True to throw a exception if validation fails.</param>
+        /// <returns>Returns true if the validation was successful.</returns>
+        public bool ValidateAuthorizationData(AuthorizationData authorizationData, bool throwExceptionOnFailure = true)
+        {
+            if (authorizationData is null)
+                throw new ArgumentNullException(nameof(authorizationData));
+            using SHA256 sha = SHA256.Create();
+            byte[] key = sha.ComputeHash(Encoding.UTF8.GetBytes(token));
+            using HMACSHA256 hmac = new(key);
+            string hash = Convert.ToHexString(hmac.ComputeHash(Encoding.UTF8.GetBytes(authorizationData.DataCheckString)));
+            if (hash != authorizationData.Hash)
+            {
+                logger?.LogCritical("Invalid authorization");
+                if (throwExceptionOnFailure)
+                    throw new TelegramBotException("Invalid authorization.");
+                return false;
+            }
+            logger?.LogCritical("Valid authorization");
+            return true;
+        }
+    }
+}
