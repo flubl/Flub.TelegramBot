5cc5b5ee66c3fa2789c09b31f260a47a5a3c7bcb
Luca Stäheli <luca.staeheli@bluewin.ch>
2021-10-04 00:32:57 +0200

    changed id property

--- Flub.TelegramBot/Types/Update.cs
+++ Flub.TelegramBot/Types/Update.cs
@@ -17,7 +17,7 @@ namespace Flub.TelegramBot.Types
         /// The update's unique identifier. Update identifiers start from a certain positive number and increase sequentially. This ID becomes especially handy if you're using Webhooks, since it allows you to ignore repeated updates or to restore the correct update sequence, should they get out of order. If there are no new updates for at least a week, then identifier of the next update will be chosen randomly instead of sequentially.
         /// </summary>
         [JsonPropertyName("update_id")]
-        public int? UpdateId { get; set; }
+        public int? Id { get; set; }
         /// <summary>
         /// Optional. New incoming message of any kind — text, photo, sticker, etc.
         /// </summary>
